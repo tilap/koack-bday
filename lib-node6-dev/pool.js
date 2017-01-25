3df5bacfe08381744486002e1d80eb8478300788
Julien Tilap LA VINH <jlavinh@gmail.com>
2017-01-25 23:58:51 +0100

    chore: replace memorystorage by mongostorage

--- lib-node6-dev/pool.js
+++ lib-node6-dev/pool.js
@@ -6,43 +6,45 @@ var _assert2 = _interopRequireDefault(_assert);
 
 var _koack = require('koack');
 
-var _memory = require('koack/storages/memory');
+var _mongo = require('koack/storages/mongo');
 
-var _memory2 = _interopRequireDefault(_memory);
+var _mongo2 = _interopRequireDefault(_mongo);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-['SLACK_CLIENTID', 'SLACK_CLIENTSECRET', 'SLACK_POOL_SIZE', 'SLACK_SCOPE'].map(envVar => (0, _assert2.default)(process.env[envVar], `Missing env configuration ${envVar}`));
+['SLACK_CLIENTID', 'SLACK_CLIENTSECRET', 'SLACK_POOL_SIZE', 'SLACK_SCOPE', 'MONGO'].map(envVar => (0, _assert2.default)(process.env[envVar], `Missing env configuration ${envVar}`));
 
-const pool = new _koack.Pool({
-  size: Number(process.env.SLACK_POOL_SIZE),
-  path: require.resolve('./bot')
-});
-
-const server = new _koack.Server({
-  pool,
-  scopes: process.env.SLACK_SCOPE.split(','),
-  slackClient: {
-    clientID: process.env.SLACK_CLIENTID,
-    clientSecret: process.env.SLACK_CLIENTSECRET
-  },
-  storage: (0, _memory2.default)()
-});
-
-server.listen({ port: Number(process.env.PORT) || 3000 });
-
-const close = (() => {
-  var _ref = _asyncToGenerator(function* () {
-    yield server.stop();
-    process.exit(0);
+_asyncToGenerator(function* () {
+  const pool = new _koack.Pool({
+    size: Number(process.env.SLACK_POOL_SIZE),
+    path: require.resolve('./bot')
   });
 
-  return function close() {
-    return _ref.apply(this, arguments);
-  };
+  const server = new _koack.Server({
+    pool,
+    scopes: process.env.SLACK_SCOPE.split(','),
+    slackClient: {
+      clientID: process.env.SLACK_CLIENTID,
+      clientSecret: process.env.SLACK_CLIENTSECRET
+    },
+    storage: yield (0, _mongo2.default)(process.env.MONGO)
+  });
+
+  server.listen({ port: Number(process.env.PORT) || 3000 });
+
+  const close = (() => {
+    var _ref2 = _asyncToGenerator(function* () {
+      yield server.stop();
+      process.exit(0);
+    });
+
+    return function close() {
+      return _ref2.apply(this, arguments);
+    };
+  })();
+  process.on('SIGINT', close);
+  process.on('SIGTERM', close);
 })();
-process.on('SIGINT', close);
-process.on('SIGTERM', close);
 //# sourceMappingURL=pool.js.map
--- src/pool.js
+++ src/pool.js
@@ -1,31 +1,33 @@
 import assert from 'assert';
 import { Pool, Server } from 'koack';
-import memoryStorage from 'koack/storages/memory';
+import mongoStorage from 'koack/storages/mongo';
 
-['SLACK_CLIENTID', 'SLACK_CLIENTSECRET', 'SLACK_POOL_SIZE', 'SLACK_SCOPE'].map(envVar =>
+['SLACK_CLIENTID', 'SLACK_CLIENTSECRET', 'SLACK_POOL_SIZE', 'SLACK_SCOPE', 'MONGO'].map(envVar =>
   assert(process.env[envVar], `Missing env configuration ${envVar}`)
 );
 
-const pool = new Pool({
-  size: Number(process.env.SLACK_POOL_SIZE),
-  path: require.resolve('./bot'),
-});
+(async () => {
+  const pool = new Pool({
+    size: Number(process.env.SLACK_POOL_SIZE),
+    path: require.resolve('./bot'),
+  });
 
-const server = new Server({
-  pool,
-  scopes: process.env.SLACK_SCOPE.split(','),
-  slackClient: {
-    clientID: process.env.SLACK_CLIENTID,
-    clientSecret: process.env.SLACK_CLIENTSECRET,
-  },
-  storage: memoryStorage(),
-});
+  const server = new Server({
+    pool,
+    scopes: process.env.SLACK_SCOPE.split(','),
+    slackClient: {
+      clientID: process.env.SLACK_CLIENTID,
+      clientSecret: process.env.SLACK_CLIENTSECRET,
+    },
+    storage: await mongoStorage(process.env.MONGO),
+  });
 
-server.listen({ port: Number(process.env.PORT) || 3000 });
+  server.listen({ port: Number(process.env.PORT) || 3000 });
 
-const close = async () => {
-  await server.stop();
-  process.exit(0);
-};
-process.on('SIGINT', close);
-process.on('SIGTERM', close);
+  const close = async () => {
+    await server.stop();
+    process.exit(0);
+  };
+  process.on('SIGINT', close);
+  process.on('SIGTERM', close);
+})();
